ee935f8be7008b23f3fefc342e7ed96a579de2de
Chris North <chris.north@astro.cf.ac.uk>
2016-05-30 00:13:09 +0100

    Added more labels and animations

--- js/plotgw-d3.js
+++ js/plotgw-d3.js
@@ -17,7 +17,8 @@ var columns = {
     initspin1:{code:"initspin1",type:"flt",avail:false},
     initspin2:{code:"initspin2",type:"flt",avail:false},
     finalspin:{code:"finalspin",type:"flt",avail:false},
-    distance:{code:"distance",type:"flt",avail:false},
+    distance:{code:"distance",type:"flt",
+        avail:false,unit:'MPc'},
     redshift:{code:"redshift",type:"flt",avail:false},
     date:{code:"date",type:"date",avail:false},
     far:{code:"far",type:"flt",avail:false},
@@ -26,7 +27,21 @@ var columns = {
     sigma:{code:"sigma",type:"flt",avail:false},
     skyarea:{code:"skyarea",type:"flt",avail:false},
 }
-
+columns.distanceLy = {
+    'type':'fn',
+    'fn':function(d){return(Math.round(d['distance']*3.26))},
+    'unit':'Mly'
+};
+columns.datestr = {
+    'type':'fn',
+    'fn':function(d){return(d['date'].split('T')[0])},
+    'unit':''
+};
+columns.timestr = {
+    'type':'fn',
+    'fn':function(d){return(d['date'].split('T')[1])},
+    'unit':''
+};
 getLabel = function(col){
     return(columns[col].label);
 }
@@ -119,19 +134,19 @@ var tttext = function(d){
 //
 // Create sketch panel
 //
-
-// add title
-var sketchName="";
-d3.select("div#sketchcontainer").append("span")
-    .attr("id","sketchtitle")
-    .html(sketchName);
+ // add title
+var sketchName="None";
+// d3.select("div#sketchcontainer").append("span")
+//     .attr("id","sketchtitle")
+//     .attr("fill","rgba(255,0,0,1)")
+//     .html(sketchName);
 
 var marginSketch = {top: 0, right: 0, bottom: 0, left: 0}
 var widthSketch = document.getElementById("sketchcontainer").offsetWidth -
     marginSketch.left - marginSketch.right;
-var heightSketch = document.getElementById("sketchcontainer").offsetHeight -
-    document.getElementById("sketchtitle").offsetHeight -
-     marginSketch.top - marginSketch.bottom;
+var heightSketch = document.getElementById("sketchcontainer").offsetHeight //-
+    // document.getElementById("sketchtitle").offsetHeight -
+    //  marginSketch.top - marginSketch.bottom;
 var aspectSketch = heightSketch/widthSketch
 
 // Add svg to sketch container
@@ -146,81 +161,219 @@ scaleRadius = function(mass,ref){return(0.2*widthSketch*(mass/100.))}
 var xScaleSk = function(x){return(x*widthSketch)}
 var xScaleSkAspect = function(x){return(x*widthSketch*aspectSketch)}
 var yScaleSk = function(y){return(y*heightSketch)}
-var colBH = "rgba(0,0,0,0.5)";
-var bhpos = {
-    pri:{cx:0.25,cy:0.1,xicon:0.1,yicon:0,xtxt:0.1,ytxt:0.1},
-    sec:{cx:0.25,cy:0.4,xicon:0.1,yicon:0.3,xtxt:0.1,ytxt:0.4},
-    final:{cx:0.6,cy:0.25,xicon:0.85,yicon:0.15,xtxt:0.85,ytxt:0.25}};
-var micon = {w:0.1,h:0.1,gap:0.005};
+// set colours
+var colBH = ["rgba(0,0,0,1)","rgba(0,0,0,0)"];
+var colShadow = ["rgba(128,128,128,1)","rgba(255,255,255,1)"];
+var gradBH = svgSketch.append("defs")
+  .append("radialGradient")
+    .attr("id", "gradBH");
+gradBH.append("stop")
+    .attr("offset", "0%")
+    .attr("stop-color", colBH[0]);
+gradBH.append("stop")
+    .attr("offset", "80%")
+    .attr("stop-color", colBH[0]);
+gradBH.append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", colBH[1]);
+var gradShadow = svgSketch.append("defs")
+  .append("radialGradient")
+    .attr("id", "gradShadow");
+gradShadow.append("stop")
+    .attr("offset", "0%")
+    .attr("stop-color", colShadow[0]);
+gradShadow.append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", colShadow[1]);
 
+// set positions
+var bhpos = {
+    pri:{cx:0.25,cy:0.3,xicon:0.1,yicon:0.15,xtxt:0.1,ytxt:0.25,scx:0.25,scy:0.3},
+    sec:{cx:0.25,cy:0.6,xicon:0.1,yicon:0.45,xtxt:0.1,ytxt:0.55,scx:0.25,scy:0.6},
+    final:{cx:0.6,cy:0.45,xicon:0.85,yicon:0.3,xtxt:0.85,ytxt:0.4,scx:0.6,scy:0.45},
+    time:{xicon:0.1,yicon:0.7,xtxt:0.2,ytxt:0.76},
+    datestr:{xicon:0.1,yicon:0.7,xtxt:0.2,ytxt:0.75},
+    timestr:{xicon:0.1,yicon:0.7,xtxt:0.2,ytxt:0.775},
+    dist:{xicon:0.1,yicon:0.85,xtxt:0.2,ytxt:0.9},
+    distly:{xicon:0.1,yicon:0.85,xtxt:0.2,ytxt:0.925}};
+var micon = {w:0.1,h:0.1};
+var icons = {
+    mass:"img/mass-msun.svg",
+    timestr:"img/mass.svg",
+    dist:"img/mass.svg"};
+var cols={
+    pri:["initmass1"],sec:["initmass2"],final:["finalmass"],
+    time:["date"],timestr:["timestr"],datestr:["datestr"],
+    dist:["distance"],distly:["distanceLy"]};
+var yout = -0.3;
+var labBlank="--";
+var sketchTitle = svgSketch.append("text")
+    .attr("x",xScaleSk(0.5))
+    .attr("y",yScaleSk(0.1))
+    .attr("text-anchor","middle")
+    .attr("font-size","2em")
+    .html("Information Panel");
 var skMasses = function(bh){
     // add mass text
-    svgSketch.append("circle")
-        .attr("class","sketch "+bh)
+    svgSketch.append("ellipse")
+        .attr("class","sketch shadow-"+bh)
         .attr("cx",xScaleSk(bhpos[bh].cx))
-        .attr("cy",yScaleSk(bhpos[bh].cy))
+        .attr("cy",yScaleSk(bhpos[bh].scy))
+        // .attr("cy",yScaleSk(yout))
+        .attr("rx",scaleRadius(1,1))
+        .attr("rx",scaleRadius(1,1))
+        .attr("fill","url(#gradShadow)");
+    svgSketch.append("circle")
+        .attr("class","sketch bh-"+bh)
+        .attr("cx",xScaleSk(bhpos[bh].cx))
+        // .attr("cy",yScaleSk(bhpos[bh].cy))
+        .attr("cy",yScaleSk(yout))
         .attr("r",scaleRadius(1,1))
-        .attr("fill",colBH);
+        .attr("fill","url(#gradBH)");
     svgSketch.append("text")
-        .attr("class","mtxt"+bh)
-        .attr("x",xScaleSk(bhpos[bh].xtxt)-xScaleSk(micon.gap))
+        .attr("class","sketchlab mtxt-"+bh)
+        .attr("font-size","1.5em")
+        .attr("x",xScaleSk(bhpos[bh].xtxt))
         .attr("y",yScaleSk(bhpos[bh].ytxt))
         .attr("dy",yScaleSk(micon.h/2.))
-        .attr("text-anchor","end")
+        .attr("text-anchor","middle")
         // .append("span")
         // .attr("class","mtxt")
-        .html("1");
+        .html(labBlank);
     // add solar mass icon
-    svgSketch.append("image")
-        .attr("xlink:href","img/msun.svg")
-        .attr("width",xScaleSk(micon.w/2)*aspectSketch)
-        .attr("height",yScaleSk(micon.h/2))
-        .attr("x",xScaleSk(bhpos[bh].xtxt)+xScaleSk(micon.gap))
-        .attr("y",yScaleSk(bhpos[bh].ytxt)+yScaleSk(micon.h/8))
-        .attr("type","image/svg+xml");
+    // svgSketch.append("image")
+    //     .attr("xlink:href","img/msun.svg")
+    //     .attr("class","msun"+bh)
+    //     .attr("width",xScaleSk(micon.w/2)*aspectSketch)
+    //     .attr("height",yScaleSk(micon.h/2))
+    //     .attr("x",xScaleSk(bhpos[bh].xtxt)+xScaleSk(micon.gap))
+    //     .attr("y",yScaleSk(bhpos[bh].ytxt)+yScaleSk(micon.h/8))
+    //     .attr("type","image/svg+xml");
     // add mass icon
     svgSketch.append("image")
-        .attr("xlink:href","img/mass.svg")
-        .attr("width",xScaleSk(micon.w))
+        .attr("xlink:href",icons.mass)
+        .attr("class","icon mass"+bh)
+        .attr("width",xScaleSkAspect(micon.w))
         .attr("height",yScaleSk(micon.h))
-        .attr("x",xScaleSk(bhpos[bh].xicon)-xScaleSkAspect(micon.w))
+        .attr("x",xScaleSk(bhpos[bh].xicon)-xScaleSkAspect(micon.w)/2)
         .attr("y",yScaleSk(bhpos[bh].yicon))
         .attr("type","image/svg+xml");
 }
+// append other icons
+addLab = function(lab){
+    svgSketch.append("image")
+        .attr("xlink:href",icons[lab])
+        .attr("class","icon "+lab)
+        .attr("width",xScaleSkAspect(micon.w))
+        .attr("height",yScaleSk(micon.h))
+        .attr("x",xScaleSk(bhpos[lab].xicon)-xScaleSkAspect(micon.w)/2)
+        .attr("y",yScaleSk(bhpos[lab].yicon)-yScaleSk(micon.h)/2)
+        .attr("type","image/svg+xml");
+    svgSketch.append("text")
+        .attr("class","sketchlab "+lab+"txt")
+        .attr("font-size","1.5em")
+        .attr("x",xScaleSk(bhpos[lab].xtxt))
+        .attr("y",yScaleSk(bhpos[lab].ytxt)-yScaleSk(micon.h)/2)
+        .html(labBlank);
+}
+
+//labels to add and keep updated
+var labels={"datestr":0,"timestr":0,"dist":0,"distly":0};
+for (lab in labels){
+    addLab(lab);
+}
+
+// define fly-in & fly-out
+var flySp=1000;
+var flyOutMasses = function(bh){
+    // fly out masses
+    if (bh=="final"){xout=1.3}else{xout=-0.3};
+    svgSketch.select('circle.bh-'+bh)
+        .transition().duration(flySp)
+        .attr("cy",yScaleSk(yout));
+    svgSketch.select('ellipse.shadow-'+bh)
+        .transition().duration(flySp)
+        .attr("rx",scaleRadius(1,1))
+        .attr("ry",scaleRadius(1,1));
+    svgSketch.select(".mtxt-"+bh).html(labBlank);
+    svgSketch.select("timetxt").html(labBlank);
+};
+var flyInMasses = function(d,bh,resize){
+    if (bh=="final"){xout=1.5}else{xout=-0.5};
+    if (resize){
+        // only resize circle & shadow
+        svgSketch.select('circle.bh-'+bh)
+            .transition().duration(flySp)
+            .attr("r",scaleRadius(d[cols[bh][0]],d["finalmass"]))
+            .attr("cy",yScaleSk(bhpos[bh].cy)-scaleRadius(d[cols[bh]],d["finalmass"]));
+        svgSketch.select('ellipse.shadow-'+bh)
+            .transition().duration(flySp)
+            .attr("rx",scaleRadius(d[cols[bh][0]],d["finalmass"]))
+            .attr("ry",scaleRadius(0.2*d[cols[bh][0]],d["finalmass"]));
+    }else{
+        // resize & fly in
+        svgSketch.select('circle.bh-'+bh)
+            .attr("r",scaleRadius(d[cols[bh][0]],d["finalmass"]));
+        svgSketch.select('circle.bh-'+bh)
+            .transition().duration(flySp).ease("bounce")
+            .attr("cx",xScaleSk(bhpos[bh].cx))
+            .attr("cy",yScaleSk(bhpos[bh].cy)-scaleRadius(d[cols[bh][0]],d["finalmass"]));
+        svgSketch.select('ellipse.shadow-'+bh)
+            .transition().duration(flySp).ease("bounce")
+            .attr("rx",scaleRadius(d[cols[bh][0]],d["finalmass"]))
+            .attr("ry",scaleRadius(0.2*d[cols[bh][0]],d["finalmass"]));
+    };
+    // set labels
+    svgSketch.select("text.mtxt-"+bh).html(d[cols[bh][0]]);
+};
 skMasses("pri");
 skMasses("sec");
 skMasses("final");
 
 updateSketch = function(d){
-    if ((document.getElementById("sketchcontainer").classList.contains("nothidden"))&&
-    (sketchName==d["name"])){
-        document.getElementById("sketchcontainer").classList.remove("nothidden");
-        document.getElementById("sketchcontainer").classList.add("hidden");
+    // if ((document.getElementById("sketchcontainer").classList.contains("nothidden"))&&
+    // (sketchName==d["name"])){
+    if (sketchName==d["name"]){
+        flyOutMasses("pri");
+        flyOutMasses("sec");
+        flyOutMasses("final");
+        // replace title
+        sketchName="None";
+        sketchTitle.html("Information Panel");
+        for (lab in labels){
+            svgSketch.select("text."+lab+"txt").html(labBlank);
+        }
     }else{
-        // update circle radii
-        svgSketch.select('circle.pri')
-         .transition().duration(200)
-         .attr("r",scaleRadius(d["initmass1"],d["finalmass"]))
-        //  .attr("cy",scaleRadius(d["initmass1"]));
-        svgSketch.select('circle.sec')
-         .transition().duration(200)
-         .attr("r",scaleRadius(d["initmass2"],d["finalmass"]))
-        //  .attr("cy",scaleRadius(d["initmass2"]));
-        svgSketch.select('circle.final')
-         .transition().duration(200)
-         .attr("r",scaleRadius(d["finalmass"],d["finalmass"]));
-        // update masses
-        svgSketch.select('text.mtxtpri')
-         .html(d["initmass1"]);
-        svgSketch.select('text.mtxtsec')
-         .html(d["initmass2"]);
-        svgSketch.select('text.mtxtfinal')
-         .html(d["finalmass"]);
-        document.getElementById("sketchcontainer").classList.remove("hidden");
-        document.getElementById("sketchcontainer").classList.add("nothidden");
+        if ((sketchName=="None")||(sketchName=="")) {
+            flyInMasses(d,"pri",false);
+            flyInMasses(d,"sec",false);
+            flyInMasses(d,"final",false);
+        }else{
+            flyInMasses(d,"pri",true);
+            flyInMasses(d,"sec",true);
+            flyInMasses(d,"final",true);
+        }
         // replace title
         sketchName = d["name"];
-        document.getElementById("sketchtitle").innerHTML = sketchName;
+        sketchTitle.html("Information: "+sketchName);
+        for (lab in labels){
+            svgSketch.select("text."+lab+"txt").html('');
+            for (i in cols[lab]){
+                svgSketch.select("text."+lab+"txt").html(
+                    svgSketch.select("text."+lab+"txt").html()+
+                        " "+d[cols[lab][i]]);
+                if (columns[cols[lab][i]].unit){
+                    svgSketch.select("text."+lab+"txt")
+                        .html(svgSketch.select("text."+lab+"txt").html()+
+                            " "+columns[cols[lab][i]].unit);
+                }
+                if (i<cols[lab].length-1){
+                    svgSketch.select("text."+lab+"txt")
+                        .html(svgSketch.select("text."+lab+"txt").html()+
+                            "<br>");
+                }
+            }
+        }
     }
 }
 
@@ -243,14 +396,19 @@ var tooltip = d3.select("div#graphcontainer").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-// load data
+//
+// load data and plot graph
+//
+
 d3.csv("csv/gwcat.csv", function(error, data) {
 
     // change string (from CSV) into number format
     var dataIn=data;
     dataIn.forEach(function(d) {
         for (col in columns){
-            if (columns[col].type=="flt"){d[col] = +d[col]}
+            if (columns[col].type=="flt"){d[col] = +d[col]};
+            if (columns[col].type=="fn"){d[col]=columns[col].fn(d)};
+            console.log(col,d[col]);
         }
     });
 
