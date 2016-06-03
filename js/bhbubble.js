de274de55f4eb653db645f7e2e9166578b5a4285
Chris North <chris.north@astro.cf.ac.uk>
2016-06-03 23:07:32 +0100

    Added links between mergers

--- js/bhbubble.js
+++ js/bhbubble.js
@@ -1,4 +1,8 @@
-var diameter = 800 //max size of the bubbles
+var diameter = Math.min(document.getElementById("bubble-container").offsetWidth,document.getElementById("bubble-container").offsetHeight);
+// document.getElementById("hdr").setAttribute("width",diameter);
+document.getElementById("bubble-container").setAttribute("width",diameter);
+console.log(document.getElementById("hdr"));
+// var diameter = 800 //max size of the bubbles
     // color    = d3.scale.category10(); //color category
 var fillcolor2 = d3.scale.linear().domain([1,2,3])
         .range([d3.rgb("#ccccFF"), d3.rgb("#0000FF"), d3.rgb('#FFFFFF')])
@@ -11,11 +15,14 @@ var legenddescs = {3:'X-rays',1:'Gravitaional Waves'}
 
 
 // random comparitor
-function comparitor(a,b){
+function randomcomparitor(a,b){
     return a.value * Math.random()- b.value*Math.random();
 }
-var dorandom=false;
-if (dorandom){
+function reversemasscomparitor(a,b){
+    return b.value - a.value;
+}
+var sort="";
+if (sort=="random"){
     var bubble = d3.layout.pack()
         .sort(comparitor)
         .size([diameter, diameter])
@@ -55,7 +62,7 @@ var tttext = function(d){
 }
 //set tooltip functions
 var showTooltip = function(d){
-    console.log('2',d.name,tooltip);
+    // console.log('2',d.name,tooltip);
     tooltip.transition()
        .duration(200)
        .style("opacity", .9);
@@ -65,6 +72,7 @@ var showTooltip = function(d){
        .style("width","auto")
        .style("height","auto");
     svg.select('#hl'+d.id)
+        .transition(500)
         .attr("stroke-opacity",1);
 }
 var hideTooltip = function(d) {
@@ -72,23 +80,33 @@ var hideTooltip = function(d) {
         .duration(500)
         .style("opacity", 0);
     svg.select('#hl'+d.id)
+        .transition(500)
         .attr("stroke-opacity",0);
 }
 
 var data;
 
-d3.json("json/bhcat.json", function(error, data){
+d3.csv("csv/bhcat.csv", function(error, data){
 
     //convert numerical values from strings to numbers
     data = data.map(function(d){ d.value = +d["massBH"]; return d; });
-    data.forEach(function(d){
-        d.id = ('hl'+d.name).replace('+','').replace('(','').replace(')','').replace('-','')
-        console.log(d.name,d.id);
-    });
+
     //bubbles needs very specific format, convert data to this.
     var nodes = bubble.nodes({children:data}).filter(function(d) { return !d.children; });
     // var nodes = bubble.nodes({children:data}).filter(function(d) { return d.method=="Xray"; });
-
+    var arrows=[];
+    var arrowpos = {};
+    data.forEach(function(d){
+        d.id = ('hl'+d.name).replace('+','').replace('(','').replace(')','').replace('-','')
+        // console.log(d.name,d.id);
+        if ((d.method=="GWinit")||(d.method=="GWfin")){
+            arrowpos[d.name]={x:d.x,y:d.y,r:d.r,c:fillcolor2(cValue(d))};
+        }
+        if (d.compType=="Black hole"){
+            arrows.push([d.name,d.compName]);
+        }
+    });
+    // console.log(arrows);
     //setup the chart
     var bubbles = svg.append("g")
         .attr("transform", "translate(0,0)")
@@ -96,6 +114,115 @@ d3.json("json/bhcat.json", function(error, data){
         .data(nodes)
         .enter();
 
+    addline = function(i){
+      x1=arrowpos[arrows[i][0]].x;
+      x2=arrowpos[arrows[i][1]].x;
+      y1=arrowpos[arrows[i][0]].y;
+      y2=arrowpos[arrows[i][1]].y;
+      svg.append("line")
+          .attr("x1",x1).attr("x2",x2).attr("y1",y1).attr("y2",y2)
+          .style({"stroke":"red","stroke-width":2});
+    }
+    addtriangle = function(i){
+      r1=arrowpos[arrows[i][0]].r;
+      r2=arrowpos[arrows[i][1]].r;
+      x1=arrowpos[arrows[i][0]].x;
+      x2=arrowpos[arrows[i][1]].x;
+      y1=arrowpos[arrows[i][0]].y;
+      y2=arrowpos[arrows[i][1]].y;
+      col=arrowpos[arrows[i][0]].c;
+    //   console.log(col);
+      ang = Math.atan2((y2-y1),(x2-x1));
+      x1r1 = x1 + r1*Math.sin(ang);
+      x1r2 = x1 - r1*Math.sin(ang);
+      y1r1 = y1 - r1*Math.cos(ang);
+      y1r2 = y1 + r1*Math.cos(ang);;
+      points = x1r1+","+y1r1+" "+x1r2+","+y1r2+" "+x2+","+y2;
+      bubbles.append("polygon")
+          .attr("points",points)
+          .attr("fill",col);
+        //   .style({"stroke":"red","stroke-width":2});
+    }
+    addpolygon = function(i){
+      r1=arrowpos[arrows[i][0]].r;
+      r2=arrowpos[arrows[i][1]].r;
+      x1=arrowpos[arrows[i][0]].x;
+      x2=arrowpos[arrows[i][1]].x;
+      y1=arrowpos[arrows[i][0]].y;
+      y2=arrowpos[arrows[i][1]].y;
+      col=arrowpos[arrows[i][0]].c;
+    //   console.log(col);
+      ang = Math.atan2((y2-y1),(x2-x1));
+      x1r1 = x1 + r1*Math.sin(ang);
+      x1r2 = x1 - r1*Math.sin(ang);
+      y1r1 = y1 - r1*Math.cos(ang);
+      y1r2 = y1 + r1*Math.cos(ang);
+      x2r1 = (r1*x2+r2*x1)/(r1+r2) + r1*Math.sin(ang)/2;
+      x2r2 = (r1*x2+r2*x1)/(r1+r2) - r1*Math.sin(ang)/2;
+      y2r1 = (r1*y2+r2*y1)/(r1+r2) - r1*Math.cos(ang)/2;
+      y2r2 = (r1*y2+r2*y1)/(r1+r2) + r1*Math.cos(ang)/2;
+      x3r1 = x2 + r2*Math.sin(ang);
+      x3r2 = x2 - r2*Math.sin(ang);
+      y3r1 = y2 - r2*Math.cos(ang);
+      y3r2 = y2 + r2*Math.cos(ang);
+      points = x1r1+","+y1r1+" "+x2r1+","+y2r1+" "+x3r1+","+y3r1+
+        " "+x3r2+","+y3r2+" "+x2r2+","+y2r2+" "+x1r2+","+y1r2;
+    //   linedata = [{"x":x1r1,"y":y1r1},]
+    //   console.log(points);
+      bubbles.append("polygon")
+          .attr("points",points)
+          .attr("fill",col);
+        //   .style({"stroke":"red","stroke-width":2});
+    }
+    addcurve = function(i){
+        r1=arrowpos[arrows[i][0]].r;
+        r2=arrowpos[arrows[i][1]].r;
+        x1=arrowpos[arrows[i][0]].x;
+        x2=arrowpos[arrows[i][1]].x;
+        y1=arrowpos[arrows[i][0]].y;
+        y2=arrowpos[arrows[i][1]].y;
+        col=arrowpos[arrows[i][0]].c;
+        //   console.log(col);
+        ang = Math.atan2((y2-y1),(x2-x1));
+        r1s=r1*Math.sin(ang);
+        r1c=r1*Math.cos(ang);
+        r2s=r2*Math.sin(ang);
+        r2c=r2*Math.cos(ang);
+        x1r1 = x1 + r1s;
+        x1r2 = x1 - r1s;
+        y1r1 = y1 - r1c;
+        y1r2 = y1 + r1c;
+        x2r1 = (r1*x2+r2*x1)/(r1+r2) + r1s/2;
+        x2r2 = (r1*x2+r2*x1)/(r1+r2) - r1s/2;
+        y2r1 = (r1*y2+r2*y1)/(r1+r2) - r1c/2;
+        y2r2 = (r1*y2+r2*y1)/(r1+r2) + r1c/2;
+        x3r1 = x2 + r2s*.75;
+        x3r2 = x2 - r2s*.75;
+        y3r1 = y2 - r2c*.75;
+        y3r2 = y2 + r2c*.75;
+        // points = "M"+x1r1+","+y1r1+" L"+x2r1+","+y2r1+" L"+x3r1+","+y3r1+
+        // " L"+x3r2+","+y3r2+" L"+x2r2+","+y2r2+" L"+x1r2+","+y1r2;
+        lineData = [{"x":x1r1,"y":y1r1},{"x":x2r1,"y":y2r1},{"x":x3r1,"y":y3r1},
+            {"x":x3r2,"y":y3r2},{"x":x2r2,"y":y2r2},{"x":x1r2,"y":y1r2}];
+        lineFunc = d3.svg.line()
+            .x(function(d){return d.x;})
+            .y(function(d){return d.y;})
+            .interpolate("cardinal")
+    //   console.log(points);
+      bubbles.append("path")
+          .attr("d",lineFunc(lineData))
+          .attr("class","merger")
+          .attr("fill",col);
+        //   .attr("opacity",0.5)
+        //   .style({"stroke":"red","stroke-width":2,"opacity":0.5});
+    }
+    for (i in arrows){
+        // addtriangle(i);
+        // addpolygon(i);
+        addcurve(i);
+    }
+    svg.selectAll("path.merger")
+        .attr("opacity",0.5);
     //create highlight circles
     bubbles.append("circle")
         .attr("r", function(d){ return d.r; })
@@ -113,15 +240,19 @@ d3.json("json/bhcat.json", function(error, data){
         .attr("cy", function(d){ return d.y; })
         .attr("id",function(d){return d.id;})
         .style("fill", function(d){return fillcolor2(cValue(d))})
+        .style({"stroke":"#000000","stoke-width":1})
         .on("mouseover", function(d) {showTooltip(d);})
         .on("mouseout", function(d) {hideTooltip(d);});
 
     //format the text for each bubble
+    getText = function(d){
+        if (d.method=="GWinit"){return "";}else{return d.name}
+    }
     bubbles.append("text")
         .attr("x", function(d){ return d.x; })
         .attr("y", function(d){ return d.y + 5; })
         .attr("text-anchor", "middle")
-        .text(function(d){ return d["name"]; })
+        .text(function(d){ return getText(d); })
         .style({
             "fill":function(d){return textcolor2(cValue(d));},
             "font-family":"Helvetica Neue, Helvetica, Arial, san-serif",
@@ -129,6 +260,10 @@ d3.json("json/bhcat.json", function(error, data){
         })
         .on("mouseover", function(d) {showTooltip(d);})
         .on("mouseout", function(d) {hideTooltip(d);;});
+    // for (i in arrows){
+    //     // addtriangle(i);
+    //     addcurve(i);
+    // }
 
     var legend = svg.selectAll(".legend")
       .data(fillcolor2.domain())
